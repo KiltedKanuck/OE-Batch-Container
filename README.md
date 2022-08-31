bb1cb5be0357ba86e37356f120e5d70cc3547eb8
Cameron Wright <cwright@progress.com>
2022-08-31 08:28:05 +0200

    More updates

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # OE-Batch-Container
 OpenEdge Batch Container
+
+BUILD 12.5.2
+./container
+docker build . -t openedge:12.5.2
+
+BUILD oebatch
+./batchClient
+docker build . -t openedge:oebatch
+RUN
+docker run -d --mount src="$(pwd)",target=/app,type=bind openedge:oebatch
--- batchClient/batchRunner.sh
+++ batchClient/batchRunner.sh
@@ -1,5 +1,24 @@
 #!/bin/bash
 
+set -e
+
+signal_handler() {
+  echo "$(date +%F_%T) INFO: Received SIGNAL. Stopping batch process ..."
+  for _ in $(seq 1 "$1"); do
+    pid=$(pidof _progres)
+    if [ -n "${pid}" ]; then
+      echo "$(date +%F_%T) INFO: Batch PID found. PID: ${pid}"
+    fi
+    sleep 1
+  done
+  kill ${pid}
+  exit ${?}
+}
+
+trap 'signal_handler' SIGTERM SIGINT
+
 export TERM=xterm
 
 $DLC/bin/pro -b -pf /app/batchRunner.pf
+
+exit 1
